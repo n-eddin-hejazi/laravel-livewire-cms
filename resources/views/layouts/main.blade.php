7edc3216a57e93e7e10739e517ebcda7a95f2736
Nour ALJELANI <89624096+nouraljelani@users.noreply.github.com>
2023-03-11 23:54:24 +0300

    build new layout

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{ config('app.name') }}</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        {{-- start fonts --}}
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Cairo&display=swap" rel="stylesheet">
+        {{-- end fonts --}}
+        <style>
+            body{
+                font-family: 'Cairo', sans-serif;
+                background-color: #F0F0F0;
+            }
+        </style>
+        @yield('style')
+    </head>
+    <body>
+        <div>
+            @include('partials.navbar')
+            <main class="py-4 mb-5">
+                <div class="container">
+                    <div class="row">
+                        {{-- @include('alerts.success') --}}
+                        @yield('content')
+                    </div>
+                </div>
+            </main>
+            @include('partials.footer')
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+        {{-- fontawesome --}}
+        <script src="https://kit.fontawesome.com/495c8a12a7.js" crossorigin="anonymous"></script>
+        @yield('script')
+    </body>
+</html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-dark w-100 p-3" style="position: absolute; bottom:0">
+    <div class="container my-auto">
+        <div class="text-center my-auto text-white">
+            <span>all rights reserved  &copy; {{ date('Y') }}</span>
+        </div>
+    </div>
+</footer>
