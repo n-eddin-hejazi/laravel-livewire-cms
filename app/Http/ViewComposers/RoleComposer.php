dd009c211e03a89040f34846fecf6d876967a0db
Nour ALJELANI <89624096+nouraljelani@users.noreply.github.com>
2023-03-17 17:03:50 +0300

    add role composer

---
+++ app/Http/ViewComposers/RoleComposer.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Http\ViewComposers;
+
+use App\Repositories\UserRepository;
+use Illuminate\View\View;
+use App\Models\Role;
+
+class RoleComposer
+{
+    protected $roles;
+
+    public function __construct(Role $roles)
+    {
+        $this->roles = $roles;
+    }
+
+    public function compose(View $view)
+    {
+        return $view->with('roles', $this->roles->all());
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use App\Http\ViewComposers\CategoryComposer;
+use App\Http\ViewComposers\RoleComposer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,5 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         view::composer(['partials.sidebar', 'lists.categories'], CategoryComposer::class);
+         view::composer('lists.roles', RoleComposer::class);
     }
 }
