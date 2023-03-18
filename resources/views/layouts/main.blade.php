3470f093d8df34a867d3c0c41b9e2e3643c0f593
Nour ALJELANI <89624096+nouraljelani@users.noreply.github.com>
2023-03-18 15:09:57 +0300

    delete footer and sidebar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -25,13 +25,11 @@
             <main class="py-4 mb-5">
                 <div class="container">
                     <div class="row">
-                        {{-- @include('alerts.success') --}}
                         @yield('content')
-                        @include('partials.sidebar')
                     </div>
                 </div>
             </main>
-            @include('partials.footer')
+            {{-- @include('partials.footer') --}}
         </div>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
