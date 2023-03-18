2344cd27d1e743a2c5106137a4be643de7c4d48d
Nour ALJELANI <89624096+nouraljelani@users.noreply.github.com>
2023-03-18 15:10:46 +0300

    fixes

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="col-md-4">
     <div class="card">
-        <h5 class="card-header">Categories</h5>
+        <h5 class="card-header p-3">Categories</h5>
         <div class="card-body">
             <ul class="nav flex-column p-0" style="list-style: none !important;">
                 <li class="nav-item">
